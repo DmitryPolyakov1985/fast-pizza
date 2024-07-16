e2322c6507b3d0fa0fe91e113c497c66efb9cf16
Dmitry <dmitry@smilecdr.com>
2024-07-15 22:43:48 -0600

    going through tailwindcss section

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,15 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 const Header = () => {
   return (
-    <header>
-      <Link to={"/"}>Fast React Pizza Co.</Link>
+    <header className="flex items-center justify-between border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to={"/"} className="tracking-[5px]">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Dmitry</p>
+      <Username />
     </header>
   );
 };
