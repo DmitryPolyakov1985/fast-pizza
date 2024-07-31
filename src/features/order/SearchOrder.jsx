0cbaa35cf3c8cdf41d81a9dbf2a0a6be84429705
Dmitry <dmitry@smilecdr.com>
2024-07-30 22:02:08 -0600

    cart is working with redux tookkit

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -18,9 +18,10 @@ const SearchOrder = () => {
   return (
     <form onSubmit={handleSubmit}>
       <input
-        placeholder="Search order number"
+        placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="rounded-full px-4 py-2 text-sm bg-yellow-100 placeholder:text-stone-400 w-29 sm:w-64 text-center sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:text-yellow-500 focus:ring-opacity-50"
       />
     </form>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    extend: {
+      colors: {
+        pizza: "#ededed",
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
